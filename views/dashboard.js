477b47caa524ea26a46b8ad9a16cc92840f5d178
Paris <bemorales@scl.edu.gt>
2025-07-22 08:06:40 -0600

    se crea la conexion con dasboard

--- views/dashboard.js
+++
@@ -1,5 +0,0 @@
-function dashboard() {
-  let dashboard = document.querySelector("#dashboard");
-  dashboard.className = "dashboard";
-  return dashboard;
-}
---
+++ views/dashboardView.js
@@ -0,0 +1,11 @@
+import { headerComponent } from "../components/header/headerComponent.js";
+function dashboard() {
+  let dashboard = document.querySelector("#dashboard");
+  dashboard.className = "dashboard";
+  dashboard.textContent = "bienvenido al dashboard";
+
+  dashboard.appendChild(headerComponent());
+  return dashboard;
+}
+
+dashboard();
